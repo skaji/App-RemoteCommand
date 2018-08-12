e1ef3b80a51be150eb2a93cd1223d8973526d3d8
Shoichi Kaji <skaji@cpan.org>
2018-08-12 20:53:37 +0900

    add configfile option

--- lib/App/RemoteCommand.pm
+++ lib/App/RemoteCommand.pm
@@ -80,6 +80,7 @@ sub parse_options {
         "append-time!"        => \(my $append_time),
         "sudo=s"              => \($self->{sudo_user}),
         "q|quiet"             => \($self->{quiet}),
+        "F=s"                 => \($self->{configfile}),
     or exit(2);
 
     my $host_arg = $host_file ? undef : shift @ARGV;
@@ -263,7 +264,7 @@ sub register {
     };
 
     for my $host (@{$self->{host}}) {
-        my $ssh = App::RemoteCommand::SSH->new($host);
+        my $ssh = App::RemoteCommand::SSH->new(host => $host, configfile => $self->{configfile});
         $ssh->add($_) for @ssh_cmd;
         $ssh->at_exit($ssh_at_exit) if $ssh_at_exit;
         push @{$self->{pending}}, $ssh;
