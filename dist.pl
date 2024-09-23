f54c288c0bdad75e4c89ae668425b7a5bb017a45
Shoichi Kaji <skaji@cpan.org>
2024-09-23 12:04:18 +0900

    update maint

---
+++ dist.pl
@@ -0,0 +1,31 @@
+my @config = (
+    name => 'App-RemoteCommand',
+
+    [
+        'Git::GatherDir' => [ exclude_filename => 'META.json', exclude_filename => 'LICENSE' ],
+        'CopyFilesFromBuild' => [ copy => 'META.json', copy => 'LICENSE' ],
+        'VersionFromMainModule' => [],
+        'LicenseFromModule' => [ override_author => 1 ],
+        'ReversionOnRelease' => [ prompt => 1 ],
+        'NextRelease' => [ format => '%v  %{yyyy-MM-dd HH:mm:ss VVV}d%{ (TRIAL RELEASE)}T' ],
+        'Git::Check' => [ allow_dirty => 'Changes', allow_dirty => 'META.json' ],
+        'GithubMeta' => [ issues => 1 ],
+        'ReadmeAnyFromPod' => [ type => 'markdown', filename => 'README.md', location => 'root' ],
+        'MetaProvides::Package' => [ inherit_version => 0, inherit_missing => 0 ],
+        'PruneFiles' => [ filename => 'dist.pl', filename => 'cpm.yml', filename => 'README.md', match => '^(xt|author|maint|example|eg)/' ],
+        'GitHubREADME::Badge' => [ badges => 'github_actions/test.yml' ],
+        'Prereqs::From::cpmfile' => [],
+        'MetaJSON' => [],
+        'Metadata' => [ x_static_install => 1 ],
+        'Git::Contributors' => [],
+        'License' => [],
+
+        'CheckChangesHasContent' => [],
+        'ConfirmRelease' => [],
+        'UploadToCPAN' => [],
+        'CopyFilesFromRelease' => [ match => '\.pm$' ],
+        'Git::Commit' => [ commit_msg => '%v', allow_dirty => 'Changes', allow_dirty => 'META.json', allow_dirty_match => '\.pm$' ],
+        'Git::Tag' => [ tag_format => '%v', tag_message => '%v' ],
+        'Git::Push' => [],
+    ],
+);
--- maint/Static.pm
+++
@@ -1,6 +0,0 @@
-package Static {
-    use v5.40;
-    use Moose;
-    with 'Dist::Zilla::Role::MetaProvider';
-    sub metadata { +{ x_static_install => 1 } }
-}
